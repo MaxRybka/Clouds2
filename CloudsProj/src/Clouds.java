5792b8b4c07c4071bcf5e8fadae161008be22a84
Max <maximrmk@gmail.com>
2019-03-02 12:24:07 +0200

    Fixed merge commit

--- CloudsProj/src/Clouds.java
+++ CloudsProj/src/Clouds.java
@@ -3,11 +3,7 @@ public class Clouds {
 
 	public static void main(String[] args) {
 		for(int i = 0; i<10 ; i++) {
-<<<<<<< HEAD
-			System.out.println(i+2);
-=======
-			System.out.println(i+1);
->>>>>>> 669be0a6d2a35d9399c5f8a7d017b275d378fc00
+			System.out.println(i+3);
 		}
 		
 	}
